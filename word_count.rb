8bc2ccb1e41f1f72149ff375669ce1a09e3e3d74
Paul Brescia <brescia717@gmail.com>
2015-01-18 21:06:13 -0500

    Ruby program that returns the number of words in a string. Listed in 'easy' section of Coderbyte.

---
+++ word_count.rb
@@ -0,0 +1,16 @@
+# Find the number of words in a string.
+print "Enter a string to get the number of words: "
+str = gets.chomp
+
+str = str.count(" ") + 1
+
+puts str
+
+
+
+# This is how the Coderbyte wants it. The above code asks you to put in your own string in the terminal.
+# str = "This is a sample."
+
+# str = str.count(" ") + 1
+
+# puts str
